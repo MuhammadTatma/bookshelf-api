65f3da23eb47d44671e07a7c50fd5f140f30b8e6
Muhammad Tatma <muhammadtatma04@gmail.com>
2023-03-22 17:18:48 +0700

    finish add book functionality

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
-const routes = [];
+const { addBook } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+];
 
 module.exports = routes;
